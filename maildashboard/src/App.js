8c8474ea58b44103c31d58cb277e6e61b9c9bf53
Rimix-incognito <75817909+Rimix-incognito@users.noreply.github.com>
2022-05-20 15:11:33 +0100

    --

--- maildashboard/src/App.js
+++ maildashboard/src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App text-gray-500">
+    <div className="body">
+      <div className='main text-gray-500 font-poppins font-extrabold text-7xl tracking-wide'>
      <h1>John Everton</h1>
+     </div>
     </div>
   );
 }
